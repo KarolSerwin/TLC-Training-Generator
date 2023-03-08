49607639c1ff1a46f7dce43bcd567dc899fdc826
Karol Serwin <serwinkarol@gmail.com>
2023-03-08 23:47:57 +0100

    added react router and links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,29 @@
-
+import React from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import './App.css';
 import {MyCycle} from "./development/MyCycle/MyCycle";
 import {CarouselMain} from "./Carousel";
 import "./carousel.css"
+import {MyProfile} from "./development/MyProfile/MyProfile";
+import {TeamCycle} from "./development/TeamCycle/TeamCycle";
+
 function App() {
-  return <>
-    <CarouselMain/>
-    <MyCycle className="mainContainer"/>
-  </>
+  return <BrowserRouter>
+    <div>
+      <CarouselMain/>
+      <ul>
+        <li><Link to="/MyProfile">My Profile</Link></li>
+        <li><Link to="/MyCycle">My Cycle</Link></li>
+        <li><Link to="/TeamCycle">Team Cycle</Link></li>
+      </ul>
+    </div>
+    <Routes>
+      <Route path="MyProfile" element={<MyProfile/>} />
+      <Route path="MyCycle" element={<MyCycle className="mainContainer"/>} />
+      <Route path="TeamCycle" element={<TeamCycle/>} />
+    </Routes>
+
+  </BrowserRouter>
 }
 
 export default App;
---
+++ my-app/src/development/TeamCycle/TeamCycle.js
@@ -0,0 +1,10 @@
+
+
+
+const TeamCycle = () => {
+
+
+    return <h1>Team Cycle</h1>
+}
+
+export {TeamCycle}
