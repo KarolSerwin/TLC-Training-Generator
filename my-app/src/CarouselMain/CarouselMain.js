078b7c767e1a68d5cfc6f25dd1281f4a2f139b2d
Karol Serwin <serwinkarol@gmail.com>
2023-03-15 21:04:31 +0100

    first part of methods section

--- my-app/src/CarouselMain/CarouselMain.js
+++ my-app/src/CarouselMain/CarouselMain.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import pic1 from "../development/Images/pic1.jpg"
-import pic2 from "../development/Images/pic2.jpg"
-import pic3 from "../development/Images/pic3.jpg"
-import pic4 from "../development/Images/pic4.jpg"
-import pic5 from "../development/Images/pic5.jpg"
-import pic6 from "../development/Images/pic6.jpg"
-import pic7 from "../development/Images/pic7.jpg"
-import pic8 from "../development/Images/pic8.jpg"
+import pic1 from "./Images/pic1.jpg"
+import pic2 from "./Images/pic2.jpg"
+import pic3 from "./Images/pic3.jpg"
+import pic4 from "./Images/pic4.jpg"
+import pic5 from "./Images/pic5.jpg"
+import pic6 from "./Images/pic6.jpg"
+import pic7 from "./Images/pic7.jpg"
+import pic8 from "./Images/pic8.jpg"
 
 import Carousel from "nuka-carousel";
 
---
+++ my-app/src/development/Methods/description.js
@@ -0,0 +1,10 @@
+
+
+const intro =
+   `If you get to this point it means that you are really interested to improve your Trials level. The Life Cycle it is a kind of philosophical trend about training. All knowledge collected here is a result of more than 20 years explorations, trials, fails and just experience. All methods are tested on highest level riders but also on beginners and kids. It is very important to learn first from this section before you will start to generate your first trainings. Better your understanding of methods will be, better profit for you. There are few basic rules you have to know: <br><br>
+    1. Every training is based on your imagination, you have to train it the same way as body. In a plan you have specific areas that you have to focus on, but realization is on your side. Be creative! <br><br>
+    2. The same plan can be done in a hundred different ways, different conditions, and still can be very fruitful.<br><br>
+    3. Be patient, as every process you will have to also learn to follow TLC trainings, every adaptation take time.`
+
+
+export {intro}
